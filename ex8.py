f6f000610f8219c7d3a32db25d58887d7015da6b
lc1915 <liuchang1915@gmail.com>
2015-07-30 17:26:40 +0800

    finish ex6 to ex10_by lc

---
+++ ex8.py
@@ -0,0 +1,17 @@
+#printing, printing
+
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+	"I had this thing.",
+	"That you could type up right.",
+	"But it didn't sing.",
+	"So I said goodnight."
+)
+
+#I tried putting Chinese (or some other non-ASCII characters) into these strings, but %r prints out weird symbols.
+#Use %s to print that instead and it'll work.
---
+++ ex9.py
@@ -0,0 +1,16 @@
+#printing, printing, printing
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+ahah
+ahhsdjkf
+hasdfka
+ashdkfalsdjfal
+alsdl
+"""
